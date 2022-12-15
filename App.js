806b83df160a5ecc369b5e77aa3ad641d0c5aa18
Tanya <t.kozlova@ongrid.pro>
2022-12-15 13:38:26 +0300

    feat: add qr code scanner

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { Text, View, StyleSheet, Button } from 'react-native'
+import { BarCodeScanner } from 'expo-barcode-scanner'
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null)
+  const [scanned, setScanned] = useState(false)
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync()
+      setHasPermission(status === 'granted')
+    }
+
+    getBarCodeScannerPermissions()
+  }, [])
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true)
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`)
+  }
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
   return (
     <View style={styles.container}>
-      <Text>QR-scanner app</Text>
-      <StatusBar style='auto' />
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
+
+      {scanned && (
+        <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+      )}
     </View>
   )
 }
@@ -13,8 +46,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 })
